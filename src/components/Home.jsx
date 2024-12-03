abbab7c68f35c0a75275388e4df9892fbaaf84e2
remygist <remy.gistelinck@student.ehb.be>
2024-12-03 11:35:26 +0100

    made homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import styles from "../styles/Home.module.css";
+
+function Home() {
+	return (
+		<>
+			<div className={styles.homeWrapper}>
+				<div className={styles.homeContainer}>
+					<div className={styles.introBlock}>
+						<h2>
+							BACKEND<br></br>
+							DEVELOPER<br></br>
+							BASED IN BRUSSELS,<br></br>
+							BELGIUM
+						</h2>
+					</div>
+					<div className={styles.introText}>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Semper fermentum odio
+							vestibulum mi tellus morbi leo. Molestie fermentum nec sit
+							vestibulum at sit in velit facilisis. Etiam tristique tellus metus
+							nunc mattis eros. Lectus orci ipsum.
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Home;
