eebcc66515004e73a5357fcf72096e552a576ea1
remygist <remy.gistelinck@student.ehb.be>
2024-12-09 00:35:37 +0100

    add intro text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,34 @@
 import styles from "../styles/Home.module.css";
 
 function Home() {
-	return (
-		<>
-			<div className={styles.homeWrapper}>
-				<div className={styles.homeContainer}>
-					<div className={styles.introBlock}>
-						<h2>
-							<span className="blueBox">BACKEND</span><br></br>
-							DEVELOPER<br></br>
-							BASED IN BRUSSELS,<br></br>
-							BELGIUM
-						</h2>
-						<div className={styles.introText}>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Semper fermentum odio
-							vestibulum mi tellus morbi leo. Molestie fermentum nec sit
-							vestibulum at sit in velit facilisis. Etiam tristique tellus metus
-							nunc mattis eros. Lectus orci ipsum.
-						</p>
-					</div>
-					</div>
-					
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className={styles.homeWrapper}>
+                <div className={styles.homeContainer}>
+                    <div className={styles.introBlock}>
+                        <h2>
+                            <span className="blueBox">BACKEND</span>
+                            <br></br>
+                            DEVELOPER<br></br>
+                            BASED IN BRUSSELS,<br></br>
+                            BELGIUM
+                        </h2>
+                        <div className={styles.introText}>
+                            <p>
+                                "Hi, I'm Rémy Gistelinck, a passionate backend
+                                developer who thrives on solving complex
+                                problems. Currently, I'm studying Multimedia and
+                                Creative Technologies at Erasmushogeschool
+                                Brussel, where I'm building a strong foundation
+                                in both software development and creative
+                                technology."
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
